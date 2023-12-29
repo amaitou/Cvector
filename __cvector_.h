512195ead3966ccaa4bad2fa1d256198b0ec87a0
Amine Ait Ouazghour <amineotaku239@gmail.com>
2023-12-29 08:08:28 +0100

    updated the ehader file

--- __cvector_.h
+++ __cvector_.h
@@ -6,7 +6,7 @@
 /*   By: amait-ou <amait-ou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/26 07:43:55 by amait-ou          #+#    #+#             */
-/*   Updated: 2023/12/29 06:26:51 by amait-ou         ###   ########.fr       */
+/*   Updated: 2023/12/29 08:08:10 by amait-ou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,13 +31,13 @@ t_vector	*__allocate_v(size_t size_t);
 void		*__get_v(t_vector *vector, size_t index);
 void		*__front_v(t_vector *vector);
 void		*__back_v(t_vector *vector);
-int			__empty_v(t_vector *vector);
 void		__free_v(t_vector *vector);
-size_t		__size_v(t_vector *vector);
+int			__empty_v(t_vector *vector);
 int			__copy_v(t_vector *old_vector, t_vector *new_vector, size_t size);
 int			__push_v(t_vector **vector, void *value, size_t size);
 int			__pop_v(t_vector *vector);
 int			__clear_v(t_vector *vector);
+size_t		__size_v(t_vector *vector);
 size_t		__capacity_v(t_vector *vector);
 
 #endif
