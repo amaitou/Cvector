a4de091679a1943035b2c67c657796cd8739ca79
Amine Ait Ouazghour <amineotaku239@gmail.com>
2023-12-29 06:26:42 +0100

    updated __cvector_h.c

--- __cvector_.h
+++ __cvector_.h
@@ -6,7 +6,7 @@
 /*   By: amait-ou <amait-ou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/26 07:43:55 by amait-ou          #+#    #+#             */
-/*   Updated: 2023/12/29 06:20:58 by amait-ou         ###   ########.fr       */
+/*   Updated: 2023/12/29 06:26:13 by amait-ou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,20 +24,20 @@ typedef struct s_vector
 	void	**vector;
 }	t_vector;
 
+void		**__begin_v(t_vector *vector);
+void		**__end_v(t_vector *vector);
 t_vector	*__create_v(void);
 t_vector	*__allocate_v(size_t size_t);
+void		*__get_v(t_vector *vector, size_t index);
+void		*__front_v(t_vector *vector);
+void		*__back_v(t_vector *vector);
 int			__empty_v(t_vector *vector);
 void		__free_v(t_vector *vector);
 size_t		__size_v(t_vector *vector);
 int			__copy_v(t_vector *old_vector, t_vector *new_vector, size_t size);
 int			__push_v(t_vector **vector, void *value, size_t size);
-void		*__get_v(t_vector *vector, size_t index);
 int			__pop_v(t_vector *vector);
-void		*__front_v(t_vector *vector);
-void		*__back_v(t_vector *vector);
 int			__clear_v(t_vector *vector);
 size_t		__capacity_v(t_vector *vector);
-void		**__begin_v(t_vector *vector);
-void		**__end_v(t_vector *vector);
 
 #endif
